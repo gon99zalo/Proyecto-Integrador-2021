f53972ed8f5ca3a50a694793d6cc70ac6d6bc7d1
BrendaCalzada <bren_c11@hotmail.com>
2021-10-20 14:09:50 -0500

    prueba-actualizacion

--- auto-app/src/components/header.jsx
+++ auto-app/src/components/header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import "../styles/header.css"
+import logo from '../assets/logo.svg';
 export default function Header() {
     return (
         <header>
             <div className="logo">
-            (logo)<span>El auto que necesitas</span>
+            <img className="logo-db" src={logo} alt="logo" /><span>El auto que necesitas</span>
             </div> 
-            <div class="inputs-header">
+            <div className="inputs-header">
             <Link to="/crearCuenta"><input className="crearCuenta" name="Crear cuenta" id="Crear cuenta" value="Crear cuenta" type="button"/></Link>
             <Link to="/iniciarSesion"><input className="iniciarSesion" name="Iniciar sesión" id="Iniciar sesión" value="Iniciar sesión" type="button"/></Link>
             </div>
