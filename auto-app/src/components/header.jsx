980a0259d73292bf17d22eff12218bd49c759295
Jorge Corredor <jcorre.es@gmail.com>
2021-10-20 14:06:54 -0500

    agregando logo

--- auto-app/src/components/header.jsx
+++ auto-app/src/components/header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom"
-import "../styles/header.css"
+import "../styles/header.css";
+import logo  from "../assets/img/logos/logo1DB.png"
+
 export default function Header() {
     return (
         <header>
             <div className="logo">
-            (logo)<span>El auto que necesitas</span>
+            <img src={logo} alt="logo" className="logo-img" />
+            <span className="slogan">El auto que necesitas</span>
             </div> 
             <div class="inputs-header">
             <Link to="/crearCuenta"><input className="crearCuenta" name="Crear cuenta" id="Crear cuenta" value="Crear cuenta" type="button"/></Link>
