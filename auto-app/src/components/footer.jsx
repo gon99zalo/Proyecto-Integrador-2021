f7e06010d3c29f14aee884d1368e4f2c99671599
santinolamberti <santilamberti4@gmail.com>
2021-10-20 16:32:06 -0300

    arreglos iconos y header estatico

--- auto-app/src/components/footer.jsx
+++ auto-app/src/components/footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
             ©2021 Digital cars
             </div>
             <div className="redes">
-            <i class="fab fa-facebook"></i> <i class="fab fa-linkedin-in"></i> <i class="fab fa-twitter"></i> <i class="fab fa-instagram"></i>
+            <i className="fab fa-facebook"></i> <i className="fab fa-linkedin-in"></i> <i className="fab fa-twitter"></i> <i className="fab fa-instagram"></i>
             </div>
         </footer>
     )
