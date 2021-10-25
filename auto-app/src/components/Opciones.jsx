919219a913d46c416424354cc2885539cecbc73d
gon99zalo <gonzalosibona@gmail.com>
2021-10-25 13:23:24 -0300

    opciones logos

--- auto-app/src/components/Opciones.jsx
+++ auto-app/src/components/Opciones.jsx
@@ -1,7 +1,14 @@
 import "../styles/Opciones.css";
 import { Link, useHistory } from "react-router-dom"
-export default function Opciones(props) {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
+const facebook = <FontAwesomeIcon icon={faFacebook} />;
+const linkedin = <FontAwesomeIcon icon={faLinkedinIn} />;
+const twitter = <FontAwesomeIcon icon={faTwitter} />;
+const instagram = <FontAwesomeIcon icon={faInstagram} />;
+
+export default function Opciones(props) {
     let history = useHistory();
     let prevLocation
     if(props.location.query === undefined){
@@ -20,6 +27,7 @@ export default function Opciones(props) {
                 {prevLocation === "iniciarSesion" ? "" : <Link to="/iniciarSesion"><h3>Iniciar sesión</h3></Link>}
                 {prevLocation === "crearCuenta" ? "" : <Link to="/crearCuenta"><h3>Crear cuenta</h3></Link>}
             </div>
+            <i>{facebook}  {linkedin}  {twitter}  {instagram}</i>
         </div>
     );
 }
