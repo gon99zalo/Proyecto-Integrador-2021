8a2071699037f8e0e3c0a83f8875be9a1de8a7f4
BrendaCalzada <bren_c11@hotmail.com>
2021-11-03 13:33:42 -0500

    comienzo de test con jest

--- Frontend/auto-app/src/components/home.jsx
+++ Frontend/auto-app/src/components/home.jsx
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <>
       <Header />
-      {/* <Buscador />
+      <Buscador />
       <Categories />
-      <Listado /> */}
+      <Listado /> 
       <Producto />
       <Footer />
     </>
