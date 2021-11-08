ca8dccd6c65a0e86fffc90ae1bf254364de7af31
BrendaCalzada <bren_c11@hotmail.com>
2021-11-08 13:49:21 -0500

    actualizado rama

--- Frontend/auto-app/src/__test__/buscador.test.js
+++ Frontend/auto-app/src/__test__/buscador.test.js
@@ -1,31 +1,16 @@
 import React from "react";
 import Buscador from "../components/Buscador";
-import { unmountComponentAtNode } from "react-dom";
+import '@testing-library/jest-dom'
+//import { unmountComponentAtNode } from "react-dom";
 import "@testing-library/jest-dom/extend-expect";
-import { render, screen, act } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 //Sprint 1
-let container = null;
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("Buscador, renderizado correcto", () => {
-  act(() => {
-    render(<Buscador />, container);
-  });
-  
-expect(screen.getByRole('button', {name: /Buscar/})).toBeInTheDocument();
-expect(screen.getByRole('button', {name: /Check in - Check out/})).toBeInTheDocument();
-expect(screen.getByRole('option', {name: /San Carlos de Bariloche/})).toBeInTheDocument();
-expect(screen.getByRole('option', {name: /Buenos Aires/})).toBeInTheDocument();
-expect(screen.getByRole('option', {name: /Mendoza/})).toBeInTheDocument();
-expect(screen.getByRole('option', {name: /Cordoba/})).toBeInTheDocument();
+describe('Buscador', () => {
+  test('Renderiza el texto', () => {
+    render(< Buscador />);
+    expect(screen.getByText(/Busca el auto que necesitas/i)).toBeInTheDocument();
+    expect(screen.getByText(/Elije donde quieres retirar el auto/i)).toBeInTheDocument();
+    expect(screen.getByText(/Check in - Check out/i)).toBeInTheDocument();
+  })
 });
