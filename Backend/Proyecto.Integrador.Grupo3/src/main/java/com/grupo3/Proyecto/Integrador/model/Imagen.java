482f2d3af968c0de6246a1428d8da02bf4f9b407
FerkCastillo <FerkSCastillo@gmail.com>
2021-11-09 13:24:55 -0300

    Actualización de back

--- Backend/Proyecto.Integrador.Grupo3/src/main/java/com/grupo3/Proyecto/Integrador/model/Imagen.java
+++ Backend/Proyecto.Integrador.Grupo3/src/main/java/com/grupo3/Proyecto/Integrador/model/Imagen.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "imagenes")
@@ -20,7 +20,7 @@ public class Imagen {
     private String url;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE, mappedBy = "imagenes")
     @JsonIgnore
-    private List<Producto> productos;
+    private List<Producto> productos = new ArrayList<>();
 
     public Imagen() { }
 
--- Backend/Proyecto.Integrador.Grupo3/src/main/java/com/grupo3/Proyecto/Integrador/repository/ProductoRepository.java
+++ Backend/Proyecto.Integrador.Grupo3/src/main/java/com/grupo3/Proyecto/Integrador/repository/ProductoRepository.java
@@ -1,26 +1,19 @@
 package com.grupo3.Proyecto.Integrador.repository;
 
-import com.grupo3.Proyecto.Integrador.model.Categoria;
-import com.grupo3.Proyecto.Integrador.model.Ciudad;
 import com.grupo3.Proyecto.Integrador.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
 
-    @Query("FROM Categoria c WHERE titulo = ?1 ORDER BY id")
-    List<Producto> listarPorCategoria(String titulo);
+    List<Producto> findAllByCategoriaTitulo(String titulo);
 
-    @Query("FROM Ciudad c WHERE nombre = ?1 ORDER BY id")
-    List<Producto> listarPorCiudad(String nombre);
+    List<Producto> findAllByCiudadNombre(String nombre);
 
     @Query("SELECT COUNT(u) FROM Producto u")
     Integer cantProductos();
