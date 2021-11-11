872edfab574a42b42711eaa456589151fb83a8ef
gon99zalo <gonzalosibona@gmail.com>
2021-11-11 12:48:18 -0300

    ; eliminado

--- Frontend/auto-app/src/components/Buscar.jsx
+++ Frontend/auto-app/src/components/Buscar.jsx
@@ -106,7 +106,7 @@ export default function Buscar(props) {
     <>
       <Header/>
       <Buscador/>
-      <div>Loading...</div>;
+      <div>Loading...</div>
       <Footer/>
     </>)
   } else {
