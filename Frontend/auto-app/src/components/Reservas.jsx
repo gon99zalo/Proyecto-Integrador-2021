9841f4fe7341f8c3769f3493ab42fbfc5c7811c6
gon99zalo <gonzalosibona@gmail.com>
2021-11-18 11:36:36 -0300

    reserva link

--- Frontend/auto-app/src/components/Reservas.jsx
+++ Frontend/auto-app/src/components/Reservas.jsx
@@ -42,7 +42,7 @@ export default function Reservas(props) {
   // AQUÍ SE TRAE LOS DATOS DEL PRODUCTO - API
   useEffect(() => {
     // Dirección de la API
-    const api = "http://localhost:8080";
+    const api = "http://ec2-3-135-186-132.us-east-2.compute.amazonaws.com:8080";
     // Aquí se hardcodeo el ID
     //------------------------------
     fetch(api + "/productos/buscar/9")
@@ -93,6 +93,7 @@ export default function Reservas(props) {
   } else {
     return (
       <>
+        <Header />
         {/* TODO: ESTE HEADER SE USO EN LA ANTERIOR SECCIÓN, PODRÍA CREARSE UN COMPONENTE PARA REUTILIZARLO */}
         <header className="booking-header">
           <div className="booking-header-titles">
@@ -214,6 +215,7 @@ export default function Reservas(props) {
             </div>
           </div>
         </div>
+        <Footer />
       </>
     );
   };
