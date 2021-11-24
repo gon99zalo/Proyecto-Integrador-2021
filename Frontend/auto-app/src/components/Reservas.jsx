de87abc62f0f98cdde1584e7b017d593fdbf822b
santinolamberti <santilamberti4@gmail.com>
2021-11-24 14:04:09 -0300

    arreglando ramta

--- Frontend/auto-app/src/components/Reservas.jsx
+++ Frontend/auto-app/src/components/Reservas.jsx
@@ -4,11 +4,7 @@ import "../styles/Reservas.css";
 //Librerías
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronLeft,
-  faMapMarkerAlt,
-  faStar,
-} from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faMapMarkerAlt, faStar } from "@fortawesome/free-solid-svg-icons";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 //Componentes
@@ -17,9 +13,6 @@ import Footer from "./Footer";
 import Loading from "./Loading";
 import FormDatos from "./FormDatos";
 import HorarioLLegada from "./HorarioLlegada";
-
-export const api =
-  "http://ec2-3-135-186-132.us-east-2.compute.amazonaws.com:8080";
 
 export default function Reservas(props) {
   // HOOKS
@@ -37,12 +30,10 @@ export default function Reservas(props) {
       nombre: "",
       pais: "",
     },
-    imagenes: [
-      {
-        titulo: "",
-        url: "",
-      },
-    ],
+    imagenes: [{
+      titulo: "",
+      url: "",
+    }],
   });
   const api = "http://ec2-3-135-186-132.us-east-2.compute.amazonaws.com:8080"
   // ÍCONOS
@@ -52,14 +43,14 @@ export default function Reservas(props) {
 
   let datosDeUsuario = sessionStorage.getItem("infoUsuario")
   let datosDeUsuarioParseado = JSON.parse(datosDeUsuario)
-  const fechaIn = document.querySelector(".hora-check-in").value
-  const fechaOut = document.querySelector(".hora-check-out").value
-  console.log(fechaIn)
+  
 
 
   const handlerReserva = (e) => {
     e.preventDefault()
-
+    const fechaIn = document.querySelector(".hora-check-in").value
+    const fechaOut = document.querySelector(".hora-check-out").value
+    console.log(fechaIn)
     let valores= {
       fechaInicial: fechaIn,
       fechaFinal: fechaOut,
@@ -86,6 +77,7 @@ export default function Reservas(props) {
   // AQUÍ SE TRAE LOS DATOS DEL PRODUCTO - API
   useEffect(() => {
     // Dirección de la API
+    const api = "http://ec2-3-135-186-132.us-east-2.compute.amazonaws.com:8080";
     fetch(api + "/productos/buscar/" + props.match.params.id)
       .then((res) => res.json())
       .then(
@@ -116,126 +108,98 @@ export default function Reservas(props) {
       );
   }, [props.match.params.id]);
   // ESTA CONDICIÓN PERMITE MOSTRAR ERROR O PANTALLA DE CARGA
-  if (!isLoaded) {
+  if (error) {
+    return (
+      <>
+        <Header />
+          <div>Error: {error.message}</div>
+        <Footer />
+      </>
+    );
+  } else if (!isLoaded) {
     return (
       <>
         <Loading />
       </>
     );
-  }
-
-  if (error) {
+  } else {
     return (
       <>
         <Header />
-        <div>Error: {error.message}</div>
-        <Footer />
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Header />
-      {/* TODO: ESTE HEADER SE USO EN LA ANTERIOR SECCIÓN, PODRÍA CREARSE UN COMPONENTE PARA REUTILIZARLO */}
-      <header className="booking-header">
-        <div className="booking-header-titles">
-          <div>
-            <h4>{producto.categoria.titulo}</h4>
-            <h1>{producto.nombre}</h1>
-          </div>
-          <i className="back-arrow">
-            <a href="/">{backArrow}</a>
-          </i>
-        </div>
-      </header>
-
-      {/* SECCIÓN DE DETALLES DEL PRODUCTO */}
-      <main className="booking-main">
-        <h1>Completá tus datos</h1>
-        <div className="booking-sections">
-          {/* DATOS PARA LA RESERVA - LADO IZQUIERDO */}
-          <div className="booking-data">
-            {/* FORMULARIO */}
-            <div
-              style={{
-                height: "217px",
-                border: "1px solid #DFE4EA",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                borderRadius: "8px",
-                marginBottom: "35px",
-              }}
-            >
-              <FormDatos />
-            </div>
-
-            {/* CALENDARIO */}
-            <h1 style={{ marginBottom: "13px" }}>
-              Seleccioná tu fecha de reserva
-            </h1>
-            <div
-              className="booking-calendar"
-              style={{
-                height: "297px",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                borderRadius: "5px",
-                marginBottom: "36px",
-              }}
-            >
-              <DatePicker inline />
-            </div>
-
-            {/* HORARIO */}
-            <h1 style={{ marginBottom: "16px" }}>Tu horario de llegada</h1>
-            <div
-              style={{
-                height: "144px",
-                border: "1px solid #DFE4EA",
-                boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                borderRadius: "8px",
-              }}
-            >
-              <HorarioLLegada horario={setHorario}/>
-            </div>
-          </div>
-
-          {/* DETALLES DE RESERVA - LADO DERECHO */}
-          <div className="booking-details">
-            <h1>Detalles de la reserva</h1>
-
-            {/* IMAGEN RESERVA */}
-            <div className="booking-details-image">
-              {console.log(producto.imagenes)}
-              <img
-                src={producto.imagenes[0].url}
-                alt={producto.imagenes[0].titulo}
-              />
-            </div>
-
-            {/* INFORMACIÓN DEL PRODUCTO */}
-            <div className="booking-details-info">
+        {/* TODO: ESTE HEADER SE USO EN LA ANTERIOR SECCIÓN, PODRÍA CREARSE UN COMPONENTE PARA REUTILIZARLO */}
+        <header className="booking-header">
+          <div className="booking-header-titles">
+            <div>
               <h4>{producto.categoria.titulo}</h4>
               <h1>{producto.nombre}</h1>
-              <div className="details-stars">
-                <i>{star}</i>
-                <i>{star}</i>
-                <i>{star}</i>
-                <i>{star}</i>
-                <i>{star}</i>
+            </div>
+            <i className="back-arrow">
+              <a href="/">{backArrow}</a>
+            </i>
+          </div>
+        </header>
+
+        {/* SECCIÓN DE DETALLES DEL PRODUCTO */}
+        <main className="booking-main">
+          <h1>Completá tus datos</h1>
+          <div className="booking-sections">
+
+            {/* DATOS PARA LA RESERVA - LADO IZQUIERDO */}
+            <div className="booking-data">
+
+              {/* FORMULARIO */}
+              <div style={{height: "217px", border: "1px solid #DFE4EA", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius: "8px", marginBottom: "35px"}}>
+                <FormDatos />
               </div>
 
-              {/* INFORMACIÓN DE LOCACIÓN DEL PRODUCTO */}
-              <div className="booking-details-location">
-                <i>{marker}</i>
-                <div>
-                  <p> A 100mt del Barrio Los Rosales</p>
-                  <p> {producto.ciudad.nombre + ", " + producto.ciudad.pais}</p>
-                </div>
+              {/* CALENDARIO */}
+              <h1 style={{marginBottom: "13px"}}>Seleccioná tu fecha de reserva</h1>
+              <div className="booking-calendar" style={{height: "297px", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius: "5px", marginBottom: "36px"}}>
+                <DatePicker inline />
+              </div>
+
+              {/* HORARIO */}
+              <h1 style={{marginBottom: "16px"}}>Tu horario de llegada</h1>
+              <div style={{height: "144px", border: "1px solid #DFE4EA", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)", borderRadius: "8px"}}>
+                <HorarioLLegada horario={setHorario}/>
               </div>
             </div>
 
-            {/* CHECK IN - CHECK OUT */}
-            <div className="booking-details-check">
+            {/* DETALLES DE RESERVA - LADO DERECHO */}
+            <div className="booking-details">
+
+              <h1>Detalles de la reserva</h1>
+
+              {/* IMAGEN RESERVA */}
+              <div className="booking-details-image">
+                {console.log(producto.imagenes)}
+                <img src={producto.imagenes[0].url} alt={producto.imagenes[0].titulo} />
+              </div>
+
+              {/* INFORMACIÓN DEL PRODUCTO */}
+              <div className="booking-details-info">
+                <h4>{producto.categoria.titulo}</h4>
+                <h1>{producto.nombre}</h1>
+                <div className="details-stars">
+                  <i>{star}</i>
+                  <i>{star}</i>
+                  <i>{star}</i>
+                  <i>{star}</i>
+                  <i>{star}</i>
+                </div>
+
+                {/* INFORMACIÓN DE LOCACIÓN DEL PRODUCTO */}
+                <div className="booking-details-location">
+                  <i>{marker}</i>
+                  <div>
+                    <p> A 100mt del Barrio Los Rosales</p>
+                    <p> {producto.ciudad.nombre + ", " + producto.ciudad.pais}</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* CHECK IN - CHECK OUT */}
+              <div className="booking-details-check">
                 <div className="booking-details-divisor"></div>
                 <div className="txt-2 checks check-in">
                   <p>Check in</p>
@@ -253,38 +217,38 @@ export default function Reservas(props) {
               <button className="btn-2 btn-details" onClick={handlerReserva}>
                 Confirmar reserva
               </button>
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
 
-      {/* REGLAS */}
-      {/* TODO: PUEDE CREARSE UN COMPONENTE Y USARLO AQUÍ Y EN LA SECCIÓN DE PRODUCTO  */}
-      <div className="commodity-rules">
-        <h1>Qué tenés que saber</h1>
-        <hr className="commodity-divisor" />
-        <div className="commodity-rule-container">
-          <div className="normas">
-            <h3>Normas del vehículo</h3>
-            <p>Norma 1</p>
-            <p>Norma 2</p>
-            <p>Norma 3</p>
-          </div>
-          <div className="salud">
-            <h3>Salud y seguridad</h3>
-            <p>Salud 1</p>
-            <p>Salud 2</p>
-            <p>Salud 3</p>
-          </div>
-          <div className="cancelacion">
-            <h3>Política de cancelación</h3>
-            <p className="texto-cancelacion">
-              Agregá las fechas de tu viaje para obtener los detalles de
-              cancelación de este vehículo.
-            </p>
+        {/* REGLAS */}
+        {/* TODO: PUEDE CREARSE UN COMPONENTE Y USARLO AQUÍ Y EN LA SECCIÓN DE PRODUCTO  */}
+        <div className="commodity-rules">
+          <h1>Qué tenés que saber</h1>
+          <hr className="commodity-divisor" />
+          <div className="commodity-rule-container">
+            <div className="normas">
+              <h3>Normas del vehículo</h3>
+              <p>Norma 1</p>
+              <p>Norma 2</p>
+              <p>Norma 3</p>
+            </div>
+            <div className="salud">
+              <h3>Salud y seguridad</h3>
+              <p>Salud 1</p>
+              <p>Salud 2</p>
+              <p>Salud 3</p>
+            </div>
+            <div className="cancelacion">
+              <h3>Política de cancelación</h3>
+              <p className="texto-cancelacion">
+                Agregá las fechas de tu viaje para obtener los detalles de cancelación de esta estadía.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer />
-    </>
-  );
-}
+        <Footer />
+      </>
+    );
+  };
+};
