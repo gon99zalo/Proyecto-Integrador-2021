00a7f36f96e4f79377376ad81f5ebe12c421dc46
joanmgs <joan.gs@outlook.com>
2021-11-24 09:23:38 -0500

    actualización

--- Frontend/auto-app/src/components/Producto.jsx
+++ Frontend/auto-app/src/components/Producto.jsx
@@ -299,35 +299,7 @@ export default function Producto(props) {
           </div>
         </div>
       </div>
-<<<<<<< HEAD
       <Politicas />
-=======
-
-      <div className="commodity-rules">
-        <h1>Qué tenés que saber</h1>
-        <hr className="commodity-divisor" />
-        <div className="commodity-rule-container">
-          <div className="normas">
-            <h3>Normas del vehículo</h3>
-            <p>Norma 1</p>
-            <p>Norma 2</p>
-            <p>Norma 3</p>
-          </div>
-          <div className="salud">
-            <h3>Salud y seguridad</h3>
-            <p>Salud 1</p>
-            <p>Salud 2</p>
-            <p>Salud 3</p>
-          </div>
-          <div className="cancelacion">
-            <h3>Política de cancelación</h3>
-            <p className="texto-cancelacion">
-              Agregá las fechas de tu viaje para obtener los detalles de cancelación de este vehículo.
-            </p>
-          </div>
-        </div>
-      </div>
->>>>>>> 257017aee398aea3e9bae9f307eeea55fec3654d
       <Footer />
     </>
   );
