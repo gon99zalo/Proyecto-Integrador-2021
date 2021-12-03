4e6a0dbe7d0f6521e0af28293494986f64025989
joanmgs <joan.gs@outlook.com>
2021-12-03 12:38:35 -0500

    agregado test de galería para revisar numero de imagenes, sus alt y src

--- Frontend/auto-app/src/__test__/gallery.test.js
+++ Frontend/auto-app/src/__test__/gallery.test.js
@@ -1,10 +1,10 @@
+//@ts-nocheck
 import React from "react";
 import "@testing-library/jest-dom";
 import Gallery from "../components/Gallery";
 import { render, screen } from "@testing-library/react";
-import { shallow, mount } from "enzyme";
+import Enzyme, { shallow, mount } from "enzyme";
 import { MemoryRouter } from "react-router";
-import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-17-updated";
 import FsLightbox from "fslightbox-react";
 
@@ -23,5 +23,35 @@ describe("<Gallery/>", () => {
   test('Renderiza div con el texto "Ver más"', () => {
     const wrapper = shallow(<Gallery imagenes={[]} />);
     expect(wrapper.find("div").first().text()).toEqual("Ver más<l />");
+  });
+  test("Renderizando de imagenes, comprueba número de imagenes, sus alt y src", () => {
+    const wrapper = shallow(<Gallery imagenes={[
+      {
+        url: "https://i.pinimg.com/originals/a7/3d/b3/a73db38375a5a59fd2774f0f8ebda49f.jpg",
+        titulo: "Sir Cat",
+      },
+      {
+        url: "https://i.pinimg.com/564x/25/fd/60/25fd60383fd1c90601b8abb07aa93187.jpg",
+        titulo: "Harry pawter",
+      }
+    ]}/>);
+    
+    // Renderiza las imagenes creadas y cuenta el número de imagenes
+    const imagenes = wrapper.find('img');
+    expect(imagenes.length).toBe(2);
+    
+    // Atrapa los alt de las imagenes y los compara con los indicados
+    const altImagenes = imagenes.map( im => im.props().alt);
+    expect(altImagenes).toEqual([
+      'Sir Cat', 
+      'Harry pawter'
+    ]);
+
+    // Atrapa los alt de las imagenes y los compara con los indicados
+    const srcImagenes = imagenes.map( im => im.props().src);
+    expect(srcImagenes).toEqual([
+      "https://i.pinimg.com/originals/a7/3d/b3/a73db38375a5a59fd2774f0f8ebda49f.jpg",
+      "https://i.pinimg.com/564x/25/fd/60/25fd60383fd1c90601b8abb07aa93187.jpg"
+    ]);
   });
 });
