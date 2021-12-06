3e3e09d720e7898fe6e1159754a1c87aaa8d7716
gon99zalo <gonzalosibona@gmail.com>
2021-12-06 10:45:42 -0300

    fix producto null

--- Frontend/auto-app/src/components/Listado.jsx
+++ Frontend/auto-app/src/components/Listado.jsx
@@ -52,22 +52,10 @@ export default function Listado() {
             .then(res => res.json())
             .then(
               (result) => {
-                setProductos(productos => [...productos, result])
+                if(result!==null)setProductos(productos => [...productos, result])
               },
               (error) => {
-                let p = {
-                  id: 0,
-                  nombre: "error",
-                  descripcion: "error",
-                  categoria: {
-                    titulo: "error",
-                  },
-                  ciudad: {
-                    nombre: "error",
-                    pais: "error"
-                  }
-                };
-                setProductos(productos => productos.push(p))
+                console.log(error);
               }
             )
           })
@@ -97,7 +85,7 @@ export default function Listado() {
             return (
                 <div className="product-card" key={i}>
                   <div className="product-image">
-                    {item.imagenes.length <= 0 ? "" : <img className="product" src={item.imagenes[0].url} alt={item.imagenes[0].titulo} />}
+                    <img className="product" src={item.imagenes[0].url} alt={item.imagenes[0].titulo} />
                     <a href="/">
                       <img className="like" src="https://buimagenes.s3.us-east-2.amazonaws.com/img/like.png" alt="like" />
                     </a>
